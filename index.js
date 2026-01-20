546c8fad3b46a5cd5fbdd9b0fafce9d6fe55ecc9
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2026-01-19 21:18:51 -0500

    Make endpoint to fetch all users

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = 3000;
 app.use(
   cors({
     origin: "*", // TODO Read from env var for production
-  })
+  }),
 );
+
+const STATUS = Object.freeze({
+  ONLINE: "ONLINE",
+  OFFLINE: "OFFLINE",
+});
 
 const USERS = JSON.parse(process.env.USERS || "{}");
 
@@ -24,20 +29,13 @@ app.get("/", async (req, res) => {
     return res
       .status(404)
       .send(
-        `Unknown user: ${user}. Make sure you are setting the ?user=[NAME] query param`
+        `Unknown user: ${user}. Make sure you are setting the ?user=[NAME] query param`,
       );
   }
 
   let result;
   try {
-    result = await fetch(
-      `${process.env.UNIFI_SCHEME}${process.env.UNIFI_DOMAIN}/proxy/network/integration/v1/sites/${process.env.UNIFI_SITE_ID}/clients?limit=100`,
-      {
-        headers: {
-          "X-API-KEY": process.env.UNIFI_API_KEY,
-        },
-      }
-    );
+    result = await fetchClients();
   } catch (err) {
     console.log("Failed fetching clients:", err);
     return res.sendStatus(500);
@@ -45,12 +43,43 @@ app.get("/", async (req, res) => {
 
   let devices = await result.json();
   let onlineDevices = devices.data.filter((device) =>
-    USERS[user].includes(device.id)
+    USERS[user].includes(device.id),
   );
 
   res.send(onlineDevices.length > 0);
 });
 
+app.get("/all", async (req, res) => {
+  let result;
+  try {
+    result = await fetchClients();
+  } catch (err) {
+    console.log("Failed fetching clients:", err);
+    return res.sendStatus(500);
+  }
+
+  let devices = await result.json();
+  let onlineDevices = devices.data.map((device) => device.id);
+  let resp = {};
+  Object.entries(USERS).forEach(([name, deviceIds]) => {
+    let online = deviceIds.some((deviceId) => onlineDevices.includes(deviceId));
+    resp[name] = online ? STATUS.ONLINE : STATUS.OFFLINE;
+  });
+
+  res.send(resp);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+const fetchClients = async () => {
+  return await fetch(
+    `${process.env.UNIFI_SCHEME}${process.env.UNIFI_DOMAIN}/proxy/network/integration/v1/sites/${process.env.UNIFI_SITE_ID}/clients?limit=100`,
+    {
+      headers: {
+        "X-API-KEY": process.env.UNIFI_API_KEY,
+      },
+    },
+  );
+};
